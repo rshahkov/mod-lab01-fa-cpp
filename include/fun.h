ca282926bc3600d14717b1308df1a2cac2b83333
rshahkov <148948376+rshahkov@users.noreply.github.com>
2024-04-13 15:52:18 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -2,8 +2,11 @@
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
 
-unsigned int faStr1(const char *str);
-unsigned int faStr2(const char *str);
-unsigned int faStr3(const char *str);
+#include <cctype>
+#include <cmath>
+
+unsigned int faStr1(const char* str);
+unsigned int faStr2(const char* str);
+unsigned int faStr3(const char* str);
 
 #endif  // INCLUDE_FUN_H_
