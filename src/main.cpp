62c36ff74811c63d09ed5204ad7b1ffc332170bc
rshahkov <148948376+rshahkov@users.noreply.github.com>
2024-04-13 15:52:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
+
 int main() {
+    using std::cout;
+    const char* string = "My favorite programming language is С#";
+    int result1 = faStr1(string);
+    int result2 = faStr2(string);
+    int result3 = faStr3(string);
+    cout << result1;
+    cout << result2;
+    cout << result3;
     return 0;
 }
